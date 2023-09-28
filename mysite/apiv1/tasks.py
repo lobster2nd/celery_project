31f5a259fe015a1c31e2840e124742e62b8306eb
lobster <lobster2@mail.ru>
2023-09-28 23:57:10 +0300

    filetype sorting implemented

--- mysite/apiv1/tasks.py
+++ mysite/apiv1/tasks.py
@@ -1,12 +1,22 @@
-from mysite.celery import app
+import os
+import shutil
 
+import magic
+
+from mysite.celery import app
 from .models import File
+
 
 @app.task
 def process_file(file_id):
     try:
         file = File.objects.get(id=file_id)
-        # Здесь вы можете выполнить обработку файла
+        file_name = os.path.basename(file.file.name)
+        file_type = magic.from_buffer(file.file.read(), mime=True)
+        directory = file_type.replace('/', '_')
+        os.makedirs("sorted_files/" + directory, exist_ok=True)
+        new_file_path = os.path.join('sorted_files/', directory)
+        shutil.copy(file.file.path, os.path.join(new_file_path, file_name))
         file.processed = True
         file.save()
         return {'status': 'success', 'message': 'Файл успешно обработан'}
--- mysite/apiv1/views.py
+++ mysite/apiv1/views.py
@@ -16,9 +16,11 @@ class FileUploadView(APIView):
         if file_serializer.is_valid():
             file = file_serializer.save()
             process_file.delay(file.id)
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+            return Response(file_serializer.data,
+                            status=status.HTTP_201_CREATED)
         else:
-            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(file_serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class FileListView(viewsets.ModelViewSet):
